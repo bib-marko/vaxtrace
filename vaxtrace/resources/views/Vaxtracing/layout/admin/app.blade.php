31d65f59a9a7fa57d773eae32d4a36aa8bfd1091
itsmeoliy <olivaresmarkanthony14@gmail.com>
2022-03-22 14:53:36 +0800

    assets

--- vaxtrace/resources/views/Vaxtracing/layout/admin/app.blade.php
+++ vaxtrace/resources/views/Vaxtracing/layout/admin/app.blade.php
@@ -25,7 +25,7 @@
 
     <!-- Icons -->
     <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
-    <link rel="shortcut icon" href="{{ asset('assets/vaxtrace_assets/assets/media/favicons/favicon.png') }}">
+    {{-- <link rel="shortcut icon" href="{{ asset('assets/vaxtrace_assets/assets/media/favicons/favicon.png') }}"> --}}
     <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('assets/vaxtrace_assets/assets/media/favicons/favicon-192x192.png') }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/vaxtrace_assets/assets/media/favicons/apple-touch-icon-180x180.png') }}">
     <!-- END Icons -->
